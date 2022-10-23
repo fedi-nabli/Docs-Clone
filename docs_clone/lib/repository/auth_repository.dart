e8c3e25f4b4faacb57af125e510444ec7f54fedc
Fedi Nabli <fedinabli@gmail.com>
2022-10-23 23:22:25 +0100

    Added client app signup functionality

--- docs_clone/lib/repository/auth_repository.dart
+++ docs_clone/lib/repository/auth_repository.dart
@@ -1,30 +1,76 @@
+import 'dart:convert';
+
+import 'package:docs_clone/constants.dart';
+import 'package:docs_clone/models/error_model.dart';
+import 'package:docs_clone/models/user_model.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:http/http.dart';
 
 final authRepositoryProvider = Provider(
   (_) => AuthRepository(
     googleSignIn: GoogleSignIn(),
+    client: Client(),
   ),
 );
 
+final userProvider = StateProvider<UserModel?>((ref) => null);
+
 class AuthRepository {
   final GoogleSignIn _googleSignIn;
+  final Client _client;
 
   AuthRepository({
     required GoogleSignIn googleSignIn,
-  }) : _googleSignIn = googleSignIn;
+    required Client client,
+  })  : _googleSignIn = googleSignIn,
+        _client = client;
 
-  void signInWithGoogle() async {
+  Future<ErrorModel> signInWithGoogle() async {
+    ErrorModel error = ErrorModel(
+      error: 'Some unexpected error happened!',
+      data: null,
+    );
+
     try {
       final user = await _googleSignIn.signIn();
 
       if (user != null) {
-        print(user.email);
-        print(user.displayName);
-        print(user.photoUrl);
+        final userAcc = UserModel(
+          name: user.displayName!,
+          email: user.email,
+          profilePic: user.photoUrl!,
+          uid: '',
+          token: '',
+        );
+
+        var res = await _client.post(
+          Uri.parse('$host/api/signup'),
+          body: userAcc.toJson(),
+          headers: {
+            'Content-Type': 'application/json; charset=UTF-8',
+          },
+        );
+
+        switch (res.statusCode) {
+          case 200:
+            final newUser = userAcc.copyWith(
+              uid: jsonDecode(res.body)['user']['_id'],
+              token: jsonDecode(res.body)['token'],
+            );
+
+            error = ErrorModel(error: null, data: newUser);
+
+            break;
+        }
       }
     } catch (e) {
-      print(e);
+      error = ErrorModel(
+        error: e.toString(),
+        data: null,
+      );
     }
+
+    return error;
   }
 }
--- docs_clone/lib/screens/login_screen.dart
+++ docs_clone/lib/screens/login_screen.dart
@@ -1,21 +1,41 @@
 import 'package:docs_clone/colors.dart';
 import 'package:docs_clone/repository/auth_repository.dart';
+import 'package:docs_clone/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class LoginScreen extends ConsumerWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
+  void signInWithGoogle(WidgetRef ref, BuildContext context) async {
+    final sMessenger = ScaffoldMessenger.of(context);
+    final navigator = Navigator.of(context);
+    final errorModel =
+        await ref.read(authRepositoryProvider).signInWithGoogle();
+    if (errorModel.error == null) {
+      ref.read(userProvider.notifier).update((state) => errorModel.data);
+      navigator.push(
+        MaterialPageRoute(
+          builder: (context) => const HomeScreen(),
+        ),
+      );
+    } else {
+      sMessenger.showSnackBar(
+        SnackBar(
+          content: Text(
+            errorModel.error!,
+          ),
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    void signInWithGoogle(WidgetRef ref) {
-      ref.read(authRepositoryProvider).signInWithGoogle();
-    }
-
     return Scaffold(
       body: Center(
         child: ElevatedButton.icon(
-          onPressed: () => signInWithGoogle(ref),
+          onPressed: () => signInWithGoogle(ref, context),
           icon: Image.asset(
             'assets/images/g-logo-2.png',
             height: 20,
