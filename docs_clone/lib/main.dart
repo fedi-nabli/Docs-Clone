c629b7ed8247b22cf29768da76996918d5c64862
Fedi Nabli <fedinabli@gmail.com>
2022-10-24 20:47:08 +0100

    Added local storage

--- docs_clone/lib/main.dart
+++ docs_clone/lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:docs_clone/models/error_model.dart';
+import 'package:docs_clone/repository/auth_repository.dart';
+import 'package:docs_clone/screens/home_screen.dart';
 import 'package:docs_clone/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -10,19 +13,41 @@ void main() {
   );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerStatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  ConsumerState<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends ConsumerState<MyApp> {
+  ErrorModel? errorModel;
+
+  @override
+  void initState() {
+    super.initState();
+    getUserData();
+  }
+
+  void getUserData() async {
+    errorModel = await ref.read(authRepositoryProvider).getUserData();
+
+    if (errorModel != null && errorModel!.data != null) {
+      ref.read(userProvider.notifier).update((state) => errorModel!.data);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    final user = ref.watch(userProvider);
+
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: LoginScreen(),
+      home: user == null ? const LoginScreen() : const HomeScreen(),
     );
   }
 }
--- docs_clone/lib/repository/auth_repository.dart
+++ docs_clone/lib/repository/auth_repository.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:docs_clone/constants.dart';
 import 'package:docs_clone/models/error_model.dart';
 import 'package:docs_clone/models/user_model.dart';
+import 'package:docs_clone/repository/local_storage_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:http/http.dart';
@@ -11,6 +12,7 @@ final authRepositoryProvider = Provider(
   (_) => AuthRepository(
     googleSignIn: GoogleSignIn(),
     client: Client(),
+    localStorageRepository: LocalStorageRepository(),
   ),
 );
 
@@ -19,12 +21,15 @@ final userProvider = StateProvider<UserModel?>((ref) => null);
 class AuthRepository {
   final GoogleSignIn _googleSignIn;
   final Client _client;
+  final LocalStorageRepository _localStorageRepository;
 
   AuthRepository({
     required GoogleSignIn googleSignIn,
     required Client client,
+    required LocalStorageRepository localStorageRepository,
   })  : _googleSignIn = googleSignIn,
-        _client = client;
+        _client = client,
+        _localStorageRepository = localStorageRepository;
 
   Future<ErrorModel> signInWithGoogle() async {
     ErrorModel error = ErrorModel(
@@ -37,9 +42,9 @@ class AuthRepository {
 
       if (user != null) {
         final userAcc = UserModel(
-          name: user.displayName!,
+          name: user.displayName ?? '',
           email: user.email,
-          profilePic: user.photoUrl!,
+          profilePic: user.photoUrl ?? '',
           uid: '',
           token: '',
         );
@@ -60,6 +65,7 @@ class AuthRepository {
             );
 
             error = ErrorModel(error: null, data: newUser);
+            _localStorageRepository.setToken(newUser.token);
 
             break;
         }
@@ -71,6 +77,43 @@ class AuthRepository {
       );
     }
 
+    return error;
+  }
+
+  Future<ErrorModel> getUserData() async {
+    ErrorModel error = ErrorModel(
+      error: 'Some unexpected error occured.',
+      data: null,
+    );
+    try {
+      String? token = await _localStorageRepository.getToken();
+
+      if (token != null) {
+        var res = await _client.get(Uri.parse('$host/'), headers: {
+          'Content-Type': 'application/json; charset=UTF-8',
+          'x-auth-token': token,
+        });
+
+        switch (res.statusCode) {
+          case 200:
+            final user = UserModel.fromJson(
+              jsonEncode(
+                jsonDecode(res.body)['user'],
+              ),
+            ).copyWith(token: token);
+
+            error = ErrorModel(error: null, data: user);
+            _localStorageRepository.setToken(user.token);
+
+            break;
+        }
+      }
+    } catch (e) {
+      error = ErrorModel(
+        error: e.toString(),
+        data: null,
+      );
+    }
     return error;
   }
 }
