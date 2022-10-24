8ba4c17176f52ceb8fc04dc6676889a0f8cf8a3f
Fedi Nabli <fedinabli@gmail.com>
2022-10-24 21:10:34 +0100

    Added navbar

--- docs_clone/lib/screens/home_screen.dart
+++ docs_clone/lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:docs_clone/colors.dart';
 import 'package:docs_clone/repository/auth_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -8,6 +9,26 @@ class HomeScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: kWhiteColor,
+        elevation: 0,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.add,
+              color: kBalckColor,
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.logout,
+              color: kRedColor,
+            ),
+          )
+        ],
+      ),
       body: Center(
         child: Text(ref.watch(userProvider)!.uid),
       ),
