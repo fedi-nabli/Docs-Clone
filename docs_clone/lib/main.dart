0de1dabc3a845ae383198441d2a86c53d937a37d
Fedi Nabli <fedinabli@gmail.com>
2022-10-24 21:05:59 +0100

    Added routermaster

--- docs_clone/lib/main.dart
+++ docs_clone/lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:docs_clone/models/error_model.dart';
 import 'package:docs_clone/repository/auth_repository.dart';
+import 'package:docs_clone/router.dart';
 import 'package:docs_clone/screens/home_screen.dart';
 import 'package:docs_clone/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:routemaster/routemaster.dart';
 
 void main() {
   runApp(
@@ -39,15 +41,22 @@ class _MyAppState extends ConsumerState<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    final user = ref.watch(userProvider);
-
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: user == null ? const LoginScreen() : const HomeScreen(),
+      routerDelegate: RoutemasterDelegate(routesBuilder: (context) {
+        final user = ref.watch(userProvider);
+
+        if (user != null && user.token.isNotEmpty) {
+          return loggedInRoute;
+        }
+
+        return loggedOutRoute;
+      }),
+      routeInformationParser: const RoutemasterParser(),
     );
   }
 }
--- docs_clone/lib/screens/login_screen.dart
+++ docs_clone/lib/screens/login_screen.dart
@@ -1,24 +1,20 @@
 import 'package:docs_clone/colors.dart';
 import 'package:docs_clone/repository/auth_repository.dart';
-import 'package:docs_clone/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:routemaster/routemaster.dart';
 
 class LoginScreen extends ConsumerWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
   void signInWithGoogle(WidgetRef ref, BuildContext context) async {
     final sMessenger = ScaffoldMessenger.of(context);
-    final navigator = Navigator.of(context);
+    final navigator = Routemaster.of(context);
     final errorModel =
         await ref.read(authRepositoryProvider).signInWithGoogle();
     if (errorModel.error == null) {
       ref.read(userProvider.notifier).update((state) => errorModel.data);
-      navigator.push(
-        MaterialPageRoute(
-          builder: (context) => const HomeScreen(),
-        ),
-      );
+      navigator.push('/');
     } else {
       sMessenger.showSnackBar(
         SnackBar(
