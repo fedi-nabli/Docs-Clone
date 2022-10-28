cde0fca4c200e824c74f1b1ce06fe1cfe8220f64
Fedi Nabli <fedinabli@gmail.com>
2022-10-28 20:23:34 +0100

    Added create document in frontend

---
+++ docs_clone/lib/repository/document_repository.dart
@@ -0,0 +1,63 @@
+import 'dart:convert';
+
+import 'package:docs_clone/constants.dart';
+import 'package:docs_clone/models/document_model.dart';
+import 'package:docs_clone/models/error_model.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:http/http.dart';
+
+final documentRepositoryProvider = Provider(
+  (ref) => DocumentRepository(
+    client: Client(),
+  ),
+);
+
+class DocumentRepository {
+  final Client _client;
+
+  DocumentRepository({
+    required Client client,
+  }) : _client = client;
+
+  Future<ErrorModel> createDocument(String token) async {
+    ErrorModel error = ErrorModel(
+      error: 'some unexpected error occured.',
+      data: null,
+    );
+
+    try {
+      var res = await _client.post(
+        Uri.parse('$host/doc/create'),
+        headers: {
+          'Content-Type': 'application/json; charset=UTF-8',
+          'x-auth-token': token,
+        },
+        body: jsonEncode({
+          'createdAt': DateTime.now().microsecondsSinceEpoch,
+        }),
+      );
+
+      switch (res.statusCode) {
+        case 200:
+          error = ErrorModel(
+            error: null,
+            data: DocumentModel.fromJson(res.body),
+          );
+          break;
+        default:
+          error = ErrorModel(
+            error: res.body,
+            data: null,
+          );
+          break;
+      }
+    } catch (e) {
+      error = ErrorModel(
+        error: e.toString(),
+        data: null,
+      );
+    }
+
+    return error;
+  }
+}
--- docs_clone/lib/screens/home_screen.dart
+++ docs_clone/lib/screens/home_screen.dart
@@ -1,7 +1,9 @@
 import 'package:docs_clone/colors.dart';
 import 'package:docs_clone/repository/auth_repository.dart';
+import 'package:docs_clone/repository/document_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:routemaster/routemaster.dart';
 
 class HomeScreen extends ConsumerWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -9,6 +11,25 @@ class HomeScreen extends ConsumerWidget {
   void signOut(WidgetRef ref) {
     ref.read(authRepositoryProvider).signOut();
     ref.read(userProvider.notifier).update((state) => null);
+  }
+
+  void createDocument(BuildContext context, WidgetRef ref) async {
+    String token = ref.read(userProvider)!.token;
+    final navigator = Routemaster.of(context);
+    final snackbar = ScaffoldMessenger.of(context);
+
+    final errorModel =
+        await ref.read(documentRepositoryProvider).createDocument(token);
+
+    if (errorModel.data != null) {
+      navigator.push('/document/${errorModel.data.id}');
+    } else {
+      snackbar.showSnackBar(
+        SnackBar(
+          content: Text(errorModel.error!),
+        ),
+      );
+    }
   }
 
   @override
@@ -19,7 +40,7 @@ class HomeScreen extends ConsumerWidget {
         elevation: 0,
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: () => createDocument(context, ref),
             icon: const Icon(
               Icons.add,
               color: kBalckColor,
