b2b0b630747643fd7c159ca2588843e87cabdd51
Fedi Nabli <fedinabli@gmail.com>
2022-10-29 00:02:50 +0100

    Added document screen design

--- docs_clone/lib/screens/document_screen.dart
+++ docs_clone/lib/screens/document_screen.dart
@@ -1,4 +1,6 @@
+import 'package:docs_clone/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_quill/flutter_quill.dart' as quill;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class DocumentScreen extends ConsumerStatefulWidget {
@@ -13,11 +15,102 @@ class DocumentScreen extends ConsumerStatefulWidget {
 }
 
 class _DocumentScreenState extends ConsumerState<DocumentScreen> {
+  TextEditingController titleController =
+      TextEditingController(text: 'Untitled Document');
+
+  final quill.QuillController _controller = quill.QuillController.basic();
+
+  @override
+  void dispose() {
+    super.dispose();
+    titleController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: kWhiteColor,
+        elevation: 0,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: ElevatedButton.icon(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.lock,
+                size: 16,
+              ),
+              label: const Text('Share'),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: kBlueColor,
+              ),
+            ),
+          ),
+        ],
+        title: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 9.0),
+          child: Row(
+            children: [
+              Image.asset(
+                'assets/images/docs-logo.png',
+                height: 40,
+              ),
+              const SizedBox(width: 10),
+              SizedBox(
+                width: 180,
+                child: TextField(
+                  controller: titleController,
+                  decoration: const InputDecoration(
+                    border: InputBorder.none,
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: kBlueColor,
+                      ),
+                    ),
+                    contentPadding: EdgeInsets.only(left: 10),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(1),
+          child: Container(
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: kGreyColor,
+                width: 0.1,
+              ),
+            ),
+          ),
+        ),
+      ),
       body: Center(
-        child: Text(widget.id),
+        child: Column(
+          children: [
+            const SizedBox(height: 10),
+            quill.QuillToolbar.basic(controller: _controller),
+            const SizedBox(height: 10),
+            Expanded(
+              child: SizedBox(
+                width: 750,
+                child: Card(
+                  color: kWhiteColor,
+                  elevation: 5,
+                  child: Padding(
+                    padding: const EdgeInsets.all(30.0),
+                    child: quill.QuillEditor.basic(
+                      controller: _controller,
+                      readOnly: false,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
