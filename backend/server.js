1d50611ed61ae91ca67bf5efd3bd9f5232990bfd
Fedi Nabli <fedinabli@gmail.com>
2022-10-29 14:47:35 +0100

    Added socket connection

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import cors from 'cors'
+import http from 'http'
+import socket from 'socket.io'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
@@ -9,15 +11,25 @@ import documentRouter from './routes/documentRoutes.js'
 
 dotenv.config()
 connectDB()
+
 const app = express()
+var server = http.createServer(app)
+var io = socket(server)
+
 app.use(cors());
 app.use(express.json())
 
 app.use(userRouter)
 app.use(documentRouter)
 
+io.on('connection', (socket) => {
+  socket.on('join', (documentId) => {
+    socket.join(documentId)
+  })
+})
+
 app.use(notFound)
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
+server.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
